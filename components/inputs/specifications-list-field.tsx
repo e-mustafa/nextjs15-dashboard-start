97eae714da988f9c4b24749d8d9c77e27e984097
e-mustafa <eng.mustafax@gmail.com>
2025-11-22 16:19:06 +0200

    specifications list field

---
+++ components/inputs/specifications-list-field.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import { FieldTypeMap, RenderFieldProps } from '@/lib/create-forms/types-create-forms';
+import { JSX } from 'react';
+import { FieldValues, Path } from 'react-hook-form';
+import { useTranslation } from 'react-i18next';
+import SpecificationsList from '../Dashboard/product-specifications';
+import { FormDescription, FormField, FormItem, FormLabel, FormMessageTranslated } from '../ui-custom/custom-form';
+
+/**
+ * Product Specifications Field
+ *
+ * @param {Object} fieldConfig - contains the field name, defaultValue, label, description, and required flag.
+ * @param {Object} form - contains the form control and values
+ * @returns {JSX.Element} - renders the product variants field component
+ */
+export default function SpecificationsListField<T extends FieldValues, K extends FieldTypeMap>({
+	fieldConfig,
+	form,
+}: RenderFieldProps<T, K>): JSX.Element {
+	const { name, label = 'forms.labels.product_specifications', description, required = false } = fieldConfig;
+	const { t } = useTranslation();
+
+	// ---------- render ----------
+	return (
+		<div className='grid gap-3'>
+			<FormField
+				control={form.control}
+				name={name}
+				render={({ field }) => {
+					return (
+						<FormItem className={fieldConfig.class}>
+							<FormLabel aria-required={!!required}>{t(label as string)}</FormLabel>
+
+							<SpecificationsList fieldName={name as Path<T>} defaultValue={field.value || []} />
+
+							{description && <FormDescription>{t(description as string)}</FormDescription>}
+							<FormMessageTranslated />
+						</FormItem>
+					);
+				}}
+			/>
+		</div>
+	);
+}
